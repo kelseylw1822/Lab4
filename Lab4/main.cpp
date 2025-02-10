03cf9f8dfa774cbb90d0e0e0fcfc65422ecf14d1
kelseylw1822 <kelseylw1822@gmail.com>
2025-02-10 11:54:24 -0500

    Initial Commit

---
+++ Lab4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab4
+//
+//  Created by Kelsey Wessel on 2/10/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
